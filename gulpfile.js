7812d8f691d358aba50160b1c9e793bddbe42963
Kimihiro Ohno <k-oono@lab-kadokawa.com>
2017-02-07 11:17:31 +0900

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var browser = require("browser-sync");
+
+//自動リロード
+gulp.task("server", function() {
+	browser({
+		server: {
+			baseDir: "./"
+		}
+	});
+});
 
 //sassコンパイル
 gulp.task("sass", function() {
 	gulp.src("./sass/**/*.scss")
 		.pipe(sass())
-		.pipe(gulp.dest("./css"));
+		.pipe(gulp.dest("./css"))
+		.pipe(browser.reload({stream:true}))
 });
 
 //自動タスク
-gulp.task('default', function() {
+gulp.task("default",['server'], function() {
 	gulp.watch('./sass/**/*.scss',['sass'])
 });
